3ce3e866412b14e04ff74021bd058e974b7a150f
Marylis <stenger_marylis@hotmail.fr>
2019-02-04 20:48:21 +0100

    ok

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,7 @@
 class StaticController < ApplicationController
 
   def home
+    @username = params["first_name"]
   end
 
   def home_post
@@ -15,6 +16,7 @@ class StaticController < ApplicationController
       puts "Salut, je suis dans le serveur"
       puts "Ceci est mon super params : #{params}"
       puts "Trop bien ! Et ceci est ce que l'utilisateur a passé dans tel champ : #{params["first_name"]}"
+      @username = params["first_name"]
   end
 
   def team
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
 
-  get '/static/home', to: 'static#home'
-  root 'static_controller#home'
+  get 'home', to: 'static#home'
+  #root 'static_controller#home'
 
-  post '/static/home', to: 'static#home_post'
-  root 'static_controller#home_post'
+  get 'home', to: 'static#home_post'
+  post 'home', to: 'static#home_post'
+  #root 'static_controller#home_post'
 
-  get '/static/home/team', to: 'static#team'
-  root 'static_controller#team'
+  get '/team', to: 'static#team'
+  #root 'static_controller#team'
 
-  get '/static/home/contact', to: 'static#contact'
-  root 'static_controller#contact'
+  get '/contact', to: 'static#contact'
+  #root 'static_controller#contact'
 
 end
